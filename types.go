e79a7207e01d9b1bbb9942d99e7066182e778824
borntodie-new <2631937454@qq.com>
2023-05-28 15:27:46 +0800

    优化types接口抽象

--- types.go
+++ types.go
@@ -2,7 +2,6 @@ package orm_framework
 
 import (
 	"context"
-	"database/sql"
 )
 
 // Querier 查询语句接口，主要是用于SELECT语句
@@ -17,7 +16,7 @@ type Querier[T any] interface {
 // 功能是执行SQL语句
 // 注意，这里不需要使用范型，因为我们并不需要将结果返回
 type Executer interface {
-	ExecuteWithContext(ctx context.Context) (sql.Result, error)
+	ExecuteWithContext(ctx context.Context) (*Result, error)
 }
 
 // Builder 构建SQL语句的的接口
@@ -31,5 +30,5 @@ type SQLInfo struct {
 	// SQL 具体的SQL语句
 	SQL string
 	// Args 具体的SQL参数
-	Args any
+	Args []any
 }
