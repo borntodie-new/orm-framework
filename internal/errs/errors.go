e1299030d0d74bd320275ec6a5e4b5b467aefd76
borntodie-new <2631937454@qq.com>
2023-05-28 12:47:45 +0800

    扩从错误类型

--- internal/errs/errors.go
+++ internal/errs/errors.go
@@ -1,7 +1,15 @@
 package errs
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	ErrNotSupportPredicate = errors.New("不支持Predicate类型")
+	ErrNotSupportModelType = errors.New("不支持模型类型")
 )
+
+func NewErrNotSupportUnknownField(val any) error {
+	return errors.New(fmt.Sprintf("不支持未知字段 %v ", val))
+}
