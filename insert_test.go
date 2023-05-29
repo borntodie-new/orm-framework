37ffe32dae17f31edf48eca9edeac6e252a2ed29
borntodie-new <2631937454@qq.com>
2023-05-29 22:20:57 +0800

    测试执行INSERT语句

--- insert_test.go
+++ insert_test.go
@@ -1,10 +1,15 @@
 package orm_framework
 
 import (
+	"context"
 	"database/sql"
+	"database/sql/driver"
+	"errors"
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/borntodie-new/orm-framework/internal/errs"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestInsertSQL_Build(t *testing.T) {
@@ -94,6 +99,56 @@ func TestInsertSQL_Build(t *testing.T) {
 				return
 			}
 			assert.Equal(t, tc.wantRes, res)
+		})
+	}
+}
+
+func TestInsertSQL_ExecuteWithContext(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	mockDB, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	db, err := OpenDB(mockDB)
+
+	testCases := []struct {
+		name       string
+		i          *InsertSQL[*TestModel]
+		prepareSQL func()
+		affected   int64
+		wantErr    error
+	}{
+		{
+			name: "no db",
+			prepareSQL: func() {
+				mock.ExpectExec("INSERT INTO .*").WillReturnError(errors.New("no db"))
+			},
+			i:       NewInsertSQL[*TestModel](db).Values(&TestModel{}),
+			wantErr: errors.New("no db"),
+		},
+		{
+			name: "affected success",
+			prepareSQL: func() {
+				result := driver.RowsAffected(19)
+				mock.ExpectExec("INSERT INTO .*").WillReturnResult(result)
+			},
+			i:        NewInsertSQL[*TestModel](db).Values(&TestModel{}, &TestModel{}),
+			affected: int64(19),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.prepareSQL()
+			res, err := tc.i.ExecuteWithContext(ctx)
+			assert.Equal(t, tc.wantErr, res.err)
+			if res.err != nil {
+				return
+			}
+			affected, err := res.RowsAffected()
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.affected, affected)
 		})
 	}
 }
