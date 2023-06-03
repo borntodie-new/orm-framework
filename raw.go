944d60d53294b12a03a8b7a1c74ab4e7a4026dd5
borntodie-new <2631937454@qq.com>
2023-06-03 13:14:29 +0800

    测试Valuer嵌入RawSQL语句中

--- raw.go
+++ raw.go
@@ -2,10 +2,9 @@ package orm_framework
 
 import (
 	"context"
-	"database/sql"
 	"github.com/borntodie-new/orm-framework/internal/errs"
+	"github.com/borntodie-new/orm-framework/internal/valuer"
 	"github.com/borntodie-new/orm-framework/model"
-	"reflect"
 )
 
 // 仅支持原生的 SELECT 语句
@@ -21,50 +20,52 @@ type RawSQL[T any] struct {
 	// db 全局的、自定义的数据库连接对象
 	db *DB
 	// models 维护一个表模型
-	models *model.Model
+	model *model.Model
+	// valuer 公共映射值方法
+	valuer valuer.FactoryValuer
 }
 
-func (r *RawSQL[T]) setFields(res *sql.Rows) (*T, error) {
-	// 最终的结果
-	tp := new(T)
-	//var t T
-	//m, err := s.db.manager.Get(t)
-	//if err != nil {
-	//	return nil, err
-	//}
-	// 重头戏——如何将SQL的结果集映射成Go中的struct
-	orderColumnsStr, err := res.Columns()
-	if err != nil {
-		return nil, err
-	}
-	// val := reflect.Indirect(reflect.ValueOf(new(T)))
-	val := reflect.ValueOf(tp).Elem()
-	receiptFields := make([]any, 0, len(r.models.Fields))                    // 保存Scan需要数据
-	receiptInterfaceFields := make([]reflect.Value, 0, len(r.models.Fields)) // 用于保存每个字段的 Value类型
-	for _, str := range orderColumnsStr {
-		fd, ok := r.models.ColumnsMap[str]
-		if !ok {
-			return nil, errs.NewErrNotSupportUnknownColumn(str)
-		}
-		temp := reflect.New(fd.Type)
-		receiptFields = append(receiptFields, temp.Interface())
-		receiptInterfaceFields = append(receiptInterfaceFields, temp.Elem())
-	}
-	// 接收SQL返回的结果数据
-	err = res.Scan(receiptFields...)
-	if err != nil {
-		return nil, err
-	}
-	// 将Scan出来的数据设置到 tp 结构体字段上
-	for idx, str := range orderColumnsStr {
-		fd, ok := r.models.ColumnsMap[str]
-		if !ok {
-			return nil, errs.NewErrNotSupportUnknownColumn(str)
-		}
-		val.FieldByName(fd.FieldName).Set(receiptInterfaceFields[idx])
-	}
-	return tp, nil
-}
+//func (r *RawSQL[T]) setFields(res *sql.Rows) (*T, error) {
+//	// 最终的结果
+//	tp := new(T)
+//	//var t T
+//	//m, err := s.db.manager.Get(t)
+//	//if err != nil {
+//	//	return nil, err
+//	//}
+//	// 重头戏——如何将SQL的结果集映射成Go中的struct
+//	orderColumnsStr, err := res.Columns()
+//	if err != nil {
+//		return nil, err
+//	}
+//	// val := reflect.Indirect(reflect.ValueOf(new(T)))
+//	val := reflect.ValueOf(tp).Elem()
+//	receiptFields := make([]any, 0, len(r.model.Fields))                    // 保存Scan需要数据
+//	receiptInterfaceFields := make([]reflect.Value, 0, len(r.model.Fields)) // 用于保存每个字段的 Value类型
+//	for _, str := range orderColumnsStr {
+//		fd, ok := r.model.ColumnsMap[str]
+//		if !ok {
+//			return nil, errs.NewErrNotSupportUnknownColumn(str)
+//		}
+//		temp := reflect.New(fd.Type)
+//		receiptFields = append(receiptFields, temp.Interface())
+//		receiptInterfaceFields = append(receiptInterfaceFields, temp.Elem())
+//	}
+//	// 接收SQL返回的结果数据
+//	err = res.Scan(receiptFields...)
+//	if err != nil {
+//		return nil, err
+//	}
+//	// 将Scan出来的数据设置到 tp 结构体字段上
+//	for idx, str := range orderColumnsStr {
+//		fd, ok := r.model.ColumnsMap[str]
+//		if !ok {
+//			return nil, errs.NewErrNotSupportUnknownColumn(str)
+//		}
+//		val.FieldByName(fd.FieldName).Set(receiptInterfaceFields[idx])
+//	}
+//	return tp, nil
+//}
 
 func (r *RawSQL[T]) QueryWithContext(ctx context.Context) ([]*T, error) {
 	// 获取 SQL 语句 和 SQL 参数
@@ -79,7 +80,9 @@ func (r *RawSQL[T]) QueryWithContext(ctx context.Context) ([]*T, error) {
 	}
 	tps := make([]*T, 0)
 	for res.Next() {
-		tp, err := r.setFields(res)
+		tp := new(T)
+		val := r.valuer(r.model, tp)
+		err = val.SetField(res)
 		if err != nil {
 			return nil, err
 		}
@@ -102,12 +105,15 @@ func (r *RawSQL[T]) QueryRawWithContext(ctx context.Context) (*T, error) {
 	if !res.Next() {
 		return nil, errs.ErrNoRows
 	}
-	return r.setFields(res)
+	tp := new(T)
+	val := r.valuer(r.model, tp)
+	err = val.SetField(res)
+	return tp, err
 }
 
 func (r *RawSQL[T]) Build() (*SQLInfo, error) {
 	var err error
-	r.models, err = r.db.manager.Get(new(T))
+	r.model, err = r.db.manager.Get(new(T))
 	if r.sql == "" {
 		return nil, errs.ErrNoSQL
 	}
@@ -117,12 +123,13 @@ func (r *RawSQL[T]) Build() (*SQLInfo, error) {
 	}, err
 }
 
-func NewRawSQL[T any](db *DB, sql string, args ...any) *RawSQL[T] {
+func NewRawSQL[T any](db *DB, valuer valuer.FactoryValuer, sql string, args ...any) *RawSQL[T] {
 	// 为什么不在这里将 model 初始化好？
 	// 为了不打断我们链式调用，因为获取 model 可能会出现错误，如果将 error 返回，就会打断链式调用
 	return &RawSQL[T]{
-		sql:  sql,
-		args: args,
-		db:   db,
+		sql:    sql,
+		args:   args,
+		db:     db,
+		valuer: valuer,
 	}
 }
